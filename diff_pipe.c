9ae51b79d2d5fe46adeb0fae3ff3e46167c3ee96
Stanley SERBIN <stanleyserbin@gmail.com>
2022-01-17 02:51:15 +0100

    stash

--- diff_pipe.c
+++ diff_pipe.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 02:36:33 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/17 02:48:42 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/17 02:50:55 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 
 void	exec_cmd(char **cmd, char **env, int in, int out)
 {
-	int		status;
-
 	if (fork() == 0)
 	{
 		if (in != -1)
@@ -37,6 +35,7 @@ int	main(int ac, char **av, char **env)
 	int		fd2[2];
 
 	pipe(fd);
+	pipe(fd2);
 	exec_cmd(cmd, env, -1, fd[1]);
 	exec_cmd(cmd2, env, fd[0], -1);
 	// exec_cmd(cmd2, env, fd[0], fd2[1]);
