94d0755140b533d271fc8844f4428c9e00e52218
Stanley SERBIN <sserbin@e2r12p15.clusters.42paris.fr>
2022-01-20 23:34:29 +0100

    norm exec_pipe.c

--- src/exec_pipe.c
+++ src/exec_pipe.c
@@ -6,11 +6,46 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 20:15:56 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/20 22:57:22 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/20 23:33:48 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+void	child_process(int fd_out, int *fd, t_token *lst, char **env)
+{
+	if (lst->out_fd != 1)
+	{
+		dup2(lst->out_fd, STDOUT_FILENO);
+		close(fd_out);
+	}
+	else if (lst->next != NULL)
+		dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	execve(lst->exec_name, lst->cmd, env);
+}
+
+int	parent_process(int *fd, int fd_in, int fd_out, t_token *lst)
+{
+	close(fd[1]);
+	if (fd_in != 0)
+		close(fd_in);
+	if (lst->in_fd)
+		fd_in = lst->in_fd;
+	else
+		fd_in = fd[0];
+	if (fd_out != 1)
+		close(fd_out);
+	return (fd_in);
+}
+
+void	set_status(int status, int *exit_status)
+{
+	if (status == 512)
+		exit_status[0] = 2;
+	if (status == 256)
+		exit_status[0] = 1;
+}
 
 void	exec_cmd(t_token *lst, char **env, int *exit_status)
 {
@@ -27,32 +62,13 @@ void	exec_cmd(t_token *lst, char **env, int *exit_status)
 		if (fork() == 0)
 		{
 			dup2(fd_in, STDIN_FILENO);
-			if (lst->out_fd != 1)
-			{
-				dup2(lst->out_fd, STDOUT_FILENO);
-				close(fd_out);
-			}
-			else if (lst->next != NULL)
-				dup2(fd[1], STDOUT_FILENO);
-			close(fd[0]);
-			execve(lst->exec_name, lst->cmd, env);
+			child_process(fd_out, fd, lst, env);
 		}
 		else
 		{
 			wait(&status);
-			if (status == 512)
-				exit_status[0] = 2;
-			if (status == 256)
-				exit_status[0] = 1;
-			close(fd[1]);
-			if (fd_in != 0)
-				close(fd_in);
-			if (lst->in_fd)
-				fd_in = lst->in_fd;
-			else
-				fd_in = fd[0];
-			if (fd_out != 1)
-				close(fd_out);
+			set_status(status, exit_status);
+			fd_in = parent_process(fd, fd_in, fd_out, lst);
 		}
 		lst = lst->next;
 	}
