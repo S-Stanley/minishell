68cd649f8c6ddd5014670b7b746d14cdc3eb0f4f
Stanley SERBIN <sserbin@e1r7p19.42.fr>
2022-01-22 18:14:29 +0100

    norm for cd ok

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 00:47:48 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/22 17:58:07 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/22 18:13:48 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,41 @@ int	find_last_index(char *str, char to_find)
 	return (index);
 }
 
+char	*get_backward_path(char *cwd)
+{
+	int		size;
+	char	*full_path;
+
+	size = find_last_index(cwd, '/') + 1;
+	full_path = malloc(sizeof(char) * size);
+	ft_strlcpy(full_path, cwd, size);
+	return (full_path);
+}
+
+char	*get_forward_path(char *cwd, char *path)
+{
+	char	*cwd_with_slash;
+	char	*full_path;
+
+	cwd_with_slash = ft_strjoin(cwd, "/");
+	full_path = ft_strjoin(cwd_with_slash, path);
+	free(cwd_with_slash);
+	return (full_path);
+}
+
 bool	builtin_cd(char *path)
 {
 	char	*full_path;
-	int		size;
 	char	*cwd;
-	char	*cwd_with_slash;
 
 	cwd = NULL;
 	cwd = getcwd(cwd, -1);
 	if (ft_strcmp(path, "..") == 0 || ft_strcmp(path, "-") == 0)
-	{
-		size = find_last_index(cwd, '/') + 1;
-		full_path = malloc(sizeof(char) * size);
-		ft_strlcpy(full_path, cwd, size);
-	}
+		full_path = get_backward_path(cwd);
 	else if (find_index(path, '/') == 0)
 		full_path = path;
 	else
-	{
-		cwd_with_slash = ft_strjoin(cwd, "/");
-		full_path = ft_strjoin(cwd_with_slash, path);
-		free(cwd_with_slash);
-	}
+		full_path = get_forward_path(cwd, path);
 	free(cwd);
 	if (chdir(full_path) == 0)
 	{
