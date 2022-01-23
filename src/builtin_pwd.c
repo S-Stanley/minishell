47a3f244c5041b811aa62a4eb269e3abc70e2b4c
Stanley SERBIN <sserbin@e2r10p9.clusters.42paris.fr>
2022-01-23 17:17:09 +0100

    fix error pwd

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 18:18:35 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/22 18:30:13 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/23 17:16:51 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	builtin_pwd(void)
 {
 	char	*buffer;
 
-	buffer = NULL;
-	buffer = getcwd(buffer, -1);
+	buffer = malloc(sizeof(char) * 100);
+	buffer = getcwd(buffer, 100);
 	printf("%s\n", buffer);
 	free(buffer);
 }
