436df94825cac5274bef7df2fd58640d97636ca7
Stanley SERBIN <sserbin@e2r10p9.clusters.42paris.fr>
2022-01-23 16:53:18 +0100

    free history ok

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 01:25:37 by rokupin           #+#    #+#             */
-/*   Updated: 2022/01/23 16:29:26 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/23 16:52:54 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,65 @@ void	exit_handler(int nb)
 	exit(0);
 }
 
+typedef struct s_history {
+	char				*str;
+	struct s_history	*next;
+}	t_history;
+
+t_history	*create_new_history(char *to_add)
+{
+	t_history	*new;
+
+	new = malloc(sizeof(t_history));
+	if (!new)
+		return (NULL);
+	new->str = to_add;
+	new->next = NULL;
+	return (new);
+}
+
+t_history	*add_cmd_line(t_history *cmd, char *to_add)
+{
+	t_history	*new;
+	t_history	*tmp;
+
+	new = create_new_history(to_add);
+	if (!new)
+		return (cmd);
+	if (!cmd)
+		return (new);
+	tmp = cmd;
+	while (cmd->next)
+		cmd = cmd->next;
+	cmd->next = new;
+	return (tmp);
+}
+
+void	free_history(t_history *history)
+{
+	t_history	*tmp;
+
+	while (history)
+	{
+		tmp = history->next;
+		free(history->str);
+		free(history);
+		history = tmp;
+	}
+}
+
 int	main(int ac, char **av, char **env)
 {
 	char		**output;
 	char		*command_line;
 	char		*prompt;
 	char		**environnement;
+	t_history	*history;
 
 	(void)av;
 	if (ac != 1)
 		return (0);
+	history = NULL;
 	g_exit_status = 0;
 	environnement = get_env(env);
 	signal(SIGINT, exit_handler);
@@ -40,17 +89,16 @@ int	main(int ac, char **av, char **env)
 	{
 		prompt = get_prompt();
 		command_line = readline(prompt);
+		history = add_cmd_line(history, command_line);
 		free(prompt);
 		if (!command_line)
-		{
-			write(1, "test\n", 5);
-			exit (0);
-		}
+			break ;
 		add_history(command_line);
 		output = ft_extract_operators(ft_extend_vars(ft_split_input(command_line), env));
 		exec(output, &environnement);
 		free_that_matrice(output);
 	}
+	free_history(history);
 	return (0);
 }
 
