17bb13b03acd0b5864495133f5ea341a9d066e58
Stanley SERBIN <sserbin@e3r8p3.clusters.42paris.fr>
2022-01-26 20:35:01 +0100

    fix prob with echo

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,31 +6,37 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 11:30:36 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/23 11:52:59 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/26 20:33:34 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	should_print_bn(char **cmd)
+bool	should_print_nl(char **cmd)
 {
-	int		i;
+	unsigned int	i;
+	unsigned int	x;
 
+	if (count_len_matrice(cmd) < 2)
+		return (true);
 	i = 1;
-	if (cmd[1][0] == '-')
+	if (cmd[i][0] != '-')
+		return (true);
+	x = 1;
+	while (cmd[i])
 	{
-		while (cmd[1][i])
+		if (i == 2)
+			break ;
+		while (cmd[i][x])
 		{
-			if (cmd[1][i] != 'n')
-			{
-				printf("\n");
-				return ;
-			}
-			i++;
+			if (cmd[i][x] == 'n')
+				x++;
+			else
+				return (true);
 		}
-		return ;
+		i++;
 	}
-	printf("\n");
+	return (false);
 }
 
 bool	builtin_echo(char **cmd)
@@ -41,15 +47,12 @@ bool	builtin_echo(char **cmd)
 	i = 1;
 	if (!cmd || count_len_matrice(cmd) == 1)
 		return (true);
-	x = 1;
-	if (cmd[i][0] == '-')
+	while (cmd[i][0] == '-' && cmd[i][1] == 'n')
 	{
+		x = 0;
 		while (cmd[i][x] == 'n')
-		{
 			x++;
-		}
-		if (!cmd[i][x])
-			i++;
+		i++;
 	}
 	while (cmd[i])
 	{
@@ -58,6 +61,7 @@ bool	builtin_echo(char **cmd)
 			printf(" ");
 		i++;
 	}
-	should_print_bn(cmd);
+	if (should_print_nl(cmd))
+		printf("\n");
 	return (true);
 }
