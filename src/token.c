95650717374d9c9ee5104c5104b65c54f2b99754
Stanley SERBIN <sserbin@e2r7p23.clusters.42paris.fr>
2022-01-31 20:27:57 +0100

    fix double free env i think

--- src/token.c
+++ src/token.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 20:15:04 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/30 19:28:03 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/31 20:27:14 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	free_token_list(t_token *lst)
 		tmp = lst->next;
 		if (ft_strcmp(lst->exec_name, "export") != 0
 			&& ft_strcmp(lst->exec_name, "unset") != 0
-			&& ft_strcmp(lst->exec_name, "cd") != 0)
+			&& ft_strcmp(lst->exec_name, "cd") != 0
+			&& ft_strcmp(lst->exec_name, "env") != 0)
 			free(lst->exec_name);
 		if (lst->infile)
 			free(lst->infile);
