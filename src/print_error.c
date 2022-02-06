b2086fc6820a6fb631a23bfb57722bf58a7311d0
Roman <planesvvalker@gmail.com>
2022-02-07 00:22:56 +0100

    replaced printf with write in error message output

--- src/print_error.c
+++ src/print_error.c
@@ -25,6 +25,19 @@ char	*get_shell(void)
 	return (shell);
 }
 
+static void		p_err(char *value, char	*shell, char *message)
+{
+	if (shell && ft_strcmp(shell, "") != 0)
+	{
+		write(STDERR_FILENO, shell, ft_strlen(shell));
+		write(STDERR_FILENO, ": ", 2);
+	}
+	write(STDERR_FILENO, value, ft_strlen(value));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, message, ft_strlen(message));
+	write(STDERR_FILENO, ": ", 2);
+}
+
 void	print_error(int code, char *value)
 {
 	char	*shell;
@@ -33,20 +46,17 @@ void	print_error(int code, char *value)
 	if (code == 0)
 	{
 		g_exit_status = 127;
-		if (shell && ft_strcmp(shell, "") != 0)
-			printf("%s: %s: command not found\n", shell, value);
-		else
-			printf("%s: command not found\n", value);
+		p_err(value, shell, "command not found\n");
 	}
 	if (code == 1)
 	{
 		g_exit_status = 1;
-		printf("%s: %s: too many arguments\n", shell, value);
+		p_err(value, shell, "too many arguments\n");
 	}
 	if (code == 2)
 	{
 		g_exit_status = 1;
-		printf("%s: %s: numeric argument required\n", shell, value);
+		p_err(value, shell, "numeric argument required\n");
 	}
 	free(shell);
 }
