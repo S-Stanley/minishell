54c70c3c24f8445a34f86b160e002e17a056e534
Stanley SERBIN <sserbin@e2r7p16.clusters.42paris.fr>
2022-02-06 19:49:07 +0100

    fix one free

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 02:18:43 by sserbin           #+#    #+#             */
-/*   Updated: 2022/02/03 20:55:14 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/02/06 19:48:06 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@ void	close_fds(void)
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(2);
+}
+
+void	free_the_rest(char **cmd)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		free(cmd[i]);
+		i++;
+	}
 }
 
 void	exec_buildint(t_token *lst, char ***env, char **cmd)
@@ -31,6 +43,7 @@ void	exec_buildint(t_token *lst, char ***env, char **cmd)
 		*env = update_env(lst->cmd, *env);
 	if (ft_strcmp(lst->cmd[0], "echo") == 0)
 		builtin_echo(lst->cmd);
+	free_the_rest(&cmd[1]);
 	free(lst->exec_name);
 	free_token_list(lst);
 	free_that_matrice(*env);
